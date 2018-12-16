a8dc44a092562ddf24687d572a19e334b7ab06aa
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-12-16 13:57:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,15 +6,17 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(name)
-attendees_array = []
-name.each { |name| attendees_array << "Hello, my name is #{name}."}
+#attendees_array = []
+#name.each { |name| attendees_array << "Hello, my name is #{name}."}
 #return attendees_array
+name.map  do |name|
+  "Hello, my name is #{name}."
 end
 
 def assign_rooms(name)
 rooms_assignements = []
 name.each_with_index {|name, index| rooms_assignements  << "Hello, #{name}! You'll be assigned to room #{index+1}!"}
-#return rooms_assignements
+return rooms_assignements
 end
 
 def printer(name)
