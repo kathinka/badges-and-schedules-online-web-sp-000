3cb85c06e8680b7aba1e61e274c64617d82f76e7
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-12-16 14:00:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,7 @@ def assign_rooms(name)
 #rooms_assignements = []
 #name.each_with_index {|name, index| rooms_assignements  << "Hello, #{name}! You'll be assigned to room #{index+1}!"}
 #return rooms_assignements
-name.each_with_index.map do |name|
+name.each_with_index.map do |name, idex|
   "Hello, #{name}! You'll be assigned to room #{index+1}!"
 end
 end
