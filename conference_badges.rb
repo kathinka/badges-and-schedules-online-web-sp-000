1887c309f4b39a3d8cb8dc1953c9a100f2ef02e6
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-12-16 14:02:54 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -24,15 +24,22 @@ end
 end
 
 def printer(name)
-  count = 0
-  attendees_array = []
-  rooms = []
-  name.each { |name| attendees_array << "Hello, my name is #{name}."}
-  name.each_with_index {|name, index| rooms  << "Hello, #{name}! You'll be assigned to room #{index+1}!"}
+#  count = 0
+#  attendees_array = []
+#  rooms = []
+#  name.each { |name| attendees_array << "Hello, my name is #{name}."}
+#  name.each_with_index {|name, index| rooms  << "Hello, #{name}! You'll be assigned to room #{index+1}!"}
 
-  while name.length > count
-  puts  attendees_array[count]
-  puts rooms[count]
-  count += 1
+#  while name.length > count
+#  puts  attendees_array[count]
+#  puts rooms[count]
+#  count += 1
+batch_badge_creator(name).each do |badge|
+  puts badge
 end
+
+assign_rooms(name).each do |assignment|
+  puts assignment
+end
+
 end
